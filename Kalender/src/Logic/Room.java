e6866be2d15ae14b9d64a558a02c338f3b8aef8c
Vegard <vegard.holter@gmail.com>
2012-03-20 13:45:20 +0100

    room class available

--- Kalender/src/Logic/Room.java
+++ Kalender/src/Logic/Room.java
@@ -4,10 +4,12 @@ public class Room {
 
 	private String name;
 	private int capasity;
+	private boolean available;
 	
 	public Room(String name, int capasity) {
 		this.name = name;
 		this.capasity = capasity;
+		available = true;
 	}
 
 	public String getName() {
@@ -26,5 +28,11 @@ public class Room {
 		this.capasity = capasity;
 	}
 	
+	public boolean isAvailable(){
+		return available;
+	}
 	
+	public void setAvailable(boolean available){
+		this.available = available;
+	}
 }

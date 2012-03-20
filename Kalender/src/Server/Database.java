8b1f3c30df73d173a8774322981764c6896c8224
Thomas Gjerde <thomasgjerde90@gmail.com>
2012-03-20 13:42:08 +0100

    Mysql driver

--- Kalender/src/Server/Database.java
+++ Kalender/src/Server/Database.java
@@ -1,8 +1,18 @@
 package Server;
 
-public class Database {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-	public static void test(){
-		
+public class Database {
+	private static Connection con;
+	public static void connect() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException{
+		final String url = "jdbc:mysql://mysql.stud.ntnu.no/" +
+		  		"thomagje_gruppe41";
+		  final String username = "thomagje_bruker";
+		  final String password = "Gruppe41";
+		  Class.forName("com.mysql.jdbc.Driver").newInstance();
+		  con = DriverManager.getConnection(url,username,password);
+		  System.out.println("Connected");
 	}
 }

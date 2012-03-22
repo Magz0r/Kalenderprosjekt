6c8215695f99d31766e4dd8611641d961ff7b5aa
Øystein Tandberg <tandberg@dhcp-083118.wlan.ntnu.no>
2012-03-22 12:37:37 +0100

    Login

--- Kalender/src/GUI/Login.java
+++ Kalender/src/GUI/Login.java
@@ -3,11 +3,13 @@ package GUI;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.HeadlessException;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -22,65 +24,65 @@ import Server.Database;
 public class Login extends JPanel implements ActionListener, KeyListener {
 
 	private JFrame frame;
-	
+
 	private JLabel usernameLabel, passwordLabel;
 	private JTextField usernameField;
 	private JPasswordField passwordField;
 	private GridBagConstraints c;
 	private JButton confirm;
-	
+
 	public Login() {
-		
+
 		frame = new JFrame();
 		frame.setTitle("Logg inn");
-		
+
 		usernameField = new JTextField(20);
 		passwordField = new JPasswordField(20);
-		
+
 		confirm = new JButton("Logg inn");
 		confirm.addActionListener(this);
 		confirm.setActionCommand("login");
-		
+
 		usernameLabel = new JLabel("Brukernavn");
 		passwordLabel = new JLabel("Passord");
 
 		usernameField.addKeyListener(this);
 		passwordField.addKeyListener(this);
-		
+
 		setPreferredSize(new Dimension(400, 250));
 		setLayout(new GridBagLayout());
 		c = new GridBagConstraints();
-		
+
 		c.gridx = 0;
 		c.gridy = 0;
 		add(usernameLabel, c);
-		
+
 		c.gridy = 0;
 		c.gridx = 1;
 		add(usernameField, c);
-		
+
 		c.gridy = 1;
 		c.gridx = 0;
 		add(passwordLabel, c);
-		
+
 		c.gridy = 1;
 		c.gridx = 1;
 		add(passwordField, c);
-		
+
 		c.gridy = 2;
 		c.gridx = 1;
 		add(confirm, c);
-		
-		
+
+
 		setVisible(true);
-		
+
 		frame.add(this);
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		new Login();
 	}
@@ -88,40 +90,37 @@ public class Login extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getActionCommand().equals("login")) {
-			System.out.println("sjekker mot server...");
 
 			String username = usernameField.getText();
 			String password = passwordField.getText();
-			
-			System.out.println(username + " - " + password);
-			
-			if(Database.login(username, password)) {
-				System.out.println("Logget inn");
-			}
-			else {
-			JOptionPane.showMessageDialog(this, "Feil brukernavn / passord");
-			}
-//			
+
+
+
+//			if(Database.login(username, password)) {
+//				new Kalenderprogram(username);
+//			}
+//			else {
+//				JOptionPane.showMessageDialog(this, "Feil brukernavn / passord");
+//			}
+
 		}
 	}
 
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		if(arg0.getKeyChar() == KeyEvent.VK_ENTER) {
-			System.out.println("sjekker mot server...");
 
 			String username = usernameField.getText();
 			String password = passwordField.getText();
-			
-			System.out.println(username + " - " + password);
-			
-			if(Database.login(username, password)) {
-				System.out.println("Logget inn");
-			}
-			else {
-			JOptionPane.showMessageDialog(this, "Feil brukernavn / passord");
-			}
-			
+
+
+//			if(Database.login(username, password)) {
+//				new Kalenderprogram(username);
+//			}
+//			else {
+//				JOptionPane.showMessageDialog(this, "Feil brukernavn / passord");
+//			}
+
 		}
 	} 
 
@@ -132,6 +131,6 @@ public class Login extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void keyTyped(KeyEvent arg0) {
 	}
-	
-	
+
+
 }

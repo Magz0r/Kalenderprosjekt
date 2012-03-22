2e03762bd58a106059e771f42dbacf8e9a803512
Øystein Tandberg <tandberg@dhcp-083118.wlan.ntnu.no>
2012-03-22 15:31:03 +0100

    date ending

--- Kalender/src/Logic/Date.java
+++ Kalender/src/Logic/Date.java
@@ -6,6 +6,8 @@ public class Date {
 
 	private long timestamp;
 	private String timeString;
+	private String date;
+	
 	
 	public Date(int year, int month, int day, int hour, int minutes) {
 		
@@ -13,7 +15,7 @@ public class Date {
 		cal.set(year, month, day, hour, minutes, 0);
 		timestamp = cal.getTimeInMillis();
 		timeString = year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":00";
-		
+		date = day + ". " + month + ". " + year;
 	}
 
 	public long getTimestamp() {
@@ -22,6 +24,21 @@ public class Date {
 	public String getTimeString() {
 		return timeString;
 	}
-	
+	public String getDate() {
+		return date;
+	}
+	public static Date toDate(String dateString) {
+		String[] ar = dateString.split(" ");
+		String[] d = ar[0].split("-");
+		String[] t = ar[1].split(":");
+		int year = Integer.parseInt(d[0]);
+		int month = Integer.parseInt(d[1]);
+		int day = Integer.parseInt(d[2]);
+		int hour = Integer.parseInt(t[0]);
+		int minute = Integer.parseInt(t[1]);
+		Date output = new Date(year, month, day, hour, minute);
+		return output;
+	}
+
 	
 }
